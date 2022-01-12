d5b03467ea9deb484dee84f3f82c65b97785aef1
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-01-13 00:08:17 +0530

    intital commit

---
+++ src/components/SearchInput.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class SearchInput extends React.Component{
+
+    // onInputChange (event){
+    //     console.log(event.target.value)
+
+    // }
+
+    constructor(props){
+        super(props)
+        this.state={entry : ''}
+    }
+    render(){
+        return(
+         
+            <div>
+                <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <input 
+                        type="search" 
+                        className="form-control" 
+                        placeholder="Search..." 
+                        aria-label="Search" 
+                        // onChange={this.onInputChange}
+                        // onChange={(event) =>console.log(event.target.value)}
+                        onChange={(event) => this.setState({entry:event.target.value})}
+                        value={this.state.entry}
+                    />
+                </form>
+              
+             </div>
+        )
+    }
+}
+export default SearchInput;

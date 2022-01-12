6466357a60d9404c77431a768c156588e1335d92
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-01-13 02:38:08 +0530

    Search input

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -6,16 +6,21 @@ class SearchInput extends React.Component{
     //     console.log(event.target.value)
 
     // }
-
     constructor(props){
         super(props)
         this.state={entry : ''}
+    }
+
+    onFormSubmit=(event)=>{
+        event.preventDefault();
+        // console.log(this.state.entry)
+        this.props.onSearchSubmit(this.state.entry)
     }
     render(){
         return(
          
             <div>
-                <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <form  onSubmit={this.onFormSubmit} className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
                     <input 
                         type="search" 
                         className="form-control" 

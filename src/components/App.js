21684d39e374c781233df35bfb2647ad330bb5dc
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-01-13 03:01:08 +0530

    installing axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SearchInput from "./SearchInput";
-
+import axios from 'axios';
 class App extends React.Component{
 
 constructor(props){
@@ -10,6 +10,7 @@ constructor(props){
 
 onSearchSubmit(entry){
     console.log(entry)
+    axios.get(`https://pixabay.com/api/?key=25220241-f47caf410736e32808efdaf70&q=${entry}&image_type=photo&pretty=true`)
 }
 
 
